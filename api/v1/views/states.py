df6fde952e5ac81ba08819d54bfeaf5a7ffbc3a9
efa07 <efatariku07@gmail.com>
2024-04-30 14:23:46 +0300

    status

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -2,14 +2,12 @@
 ''' new view for State objects'''
 
 from api.v1.views import app_views
-from flask import Flask
-from flask import Flask, abort
+from flask import Flask, abort, request, jsonify
 from models.state import State
-from flask import request
 from os import name
 
 
-@app_views.route('/status', methods=['GET'] strict_slashes=False)
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
 def toGet():
     '''getting thing'''
     objects = storage.all('State')
@@ -19,11 +17,11 @@ def toGet():
     return jsonify(lista)
 
 
-@app_views.route('/states/<string:stateid>', methods=['GET'],
+@app_views.route('/states/<string:state_id>', methods=['GET'],
                  strict_slashes=False)
-def toGetid():
+def toGetid(state_id):
     '''Updates a State object id'''
-    objects = storage.get('State', 'state_id')
+    objects = storage.get('State', state_id)
     if objects is None:
         abort(404)
     return jsonify(objects.to_dict()), 'OK'
@@ -34,10 +32,10 @@ def toGetid():
 def posting():
     '''Creates a State'''
     response = request.get_json()
-    if response id None:
-        abort(400, {'Not a JSON'})
-    if "name" not in response:
-        abort(400, {'Missing name'})
+    if response is None:
+        abort(400, {'error': 'Not a JSON'})
+    if 'name' not in response:
+        abort(400, {'error': 'Missing name'})
     stateObject = State(name=response['name'])
     storage.new(stateObject)
     storage.save()
@@ -46,26 +44,26 @@ def posting():
 
 @app_views.route('/states/<state_id>', methods=['PUT'],
                  strict_slashes=False)
-def putinV():
+def putinV(state_id):
     '''vladimir'''
     response = request.get_json()
-    if response id None:
-        abort(400, {'Not a JSON'})
+    if response is None:
+        abort(400, {'error': 'Not a JSON'})
     stateObject = storage.get(State, state_id)
     if stateObject is None:
         abort(404)
     ignoreKeys = ['id', 'created_at', 'updated_at']
-    for key in response.items():
+    for key, value in response.items():
         if key not in ignoreKeys:
-            setattr(stateObject, key)
+            setattr(stateObject, key, value)
     storage.save()
     return jsonify(stateObject.to_dict()), '200'
 
 
 @app_views.route('/states/<state_id>', methods=['DELETE'],
                  strict_slashes=False)
-def deleting():
-    ''' to delete an onbject'''
+def deleting(state_id):
+    ''' to delete an object'''
     stateObject = storage.get(State, state_id)
     if stateObject is None:
         abort(404)
